c81e802bfb08f7c0967c9dc9ca38fea1423e698d
Faiq0005 <123771056+Faiq0005@users.noreply.github.com>
2024-12-11 12:17:58 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ================== typing animation =================== */
 var typed = new Typed(".typing", {
-    strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "Freelancer"],
+    strings: ["", "Web Developer", "Graphic Designer", "Freelancer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
